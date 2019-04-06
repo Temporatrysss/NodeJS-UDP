1b77385bec78ea57254a4f0ce2c2e96f7d463129
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2019-04-06 18:04:37 +0800

    add random

--- client.js
+++ client.js
@@ -5,9 +5,18 @@ socket.bind(function () {
   socket.setBroadcast(true);
 });
 
+const randomInt = (a, b) => { // 取得 a 到 b 之間的一個整數亂數
+  return Math.floor(a + Math.random() * (b - a));
+}
+
 
 setInterval(function(){
-  const message = Buffer.from("0:1:2:3:4", 'utf8');
+  let data1 = randomInt(0, 10); //1 Attention
+  let data2 = randomInt(0, 10); //2 Emotional Level
+  let data3 = randomInt(0, 10); //3 Fatigue
+  let data4 = randomInt(0, 10); //4 Stress
+  let data5 = randomInt(0, 10); //5 Rational Level
+  const message = Buffer.from(`${data1}:${data2}:${data3}:${data4}:${data5}`, 'utf8');
   socket.send(message, 0, message.length, 5000, '255.255.255.255', function (err, bytes) {
     
   });
